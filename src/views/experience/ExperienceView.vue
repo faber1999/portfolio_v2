d957369056717a53ca1800f597d0bf704833b70e
Faber Grajales <fabergrajales1999@gmail.com>
2025-03-29 13:43:55 -0500

    feat: Add experience section and new language icons; update skill progress layout

--- src/views/experience/ExperienceView.vue
+++ src/views/experience/ExperienceView.vue
@@ -1,7 +1,45 @@
 <script setup lang="ts">
-import ContentUnderConstruction from '@/components/ContentUnderConstruction.vue'
+import IconBuilderStar from '@/components/icons/IconBuilderStar.vue'
+import ExperienceItem from './components/ExperienceItem.vue'
+
+const experience = [
+  {
+    position: 'experience.job-3-position',
+    company: 'Celuweb.com S.A.S.',
+    period: [2021, 2022],
+    descriptions: ['experience.job-3-description-1', 'experience.job-3-description-2'],
+  },
+  {
+    position: 'experience.job-2-position',
+    company: 'Sophos Solutions',
+    period: [2020, 2021],
+    descriptions: ['experience.job-2-description-1', 'experience.job-2-description-2'],
+  },
+  {
+    position: 'experience.job-1-position',
+    company: 'Celuweb.com S.A.S.',
+    period: [2021, 2022],
+    descriptions: ['experience.job-1-description-1', 'experience.job-1-description-2'],
+  },
+]
 </script>
 
 <template>
-  <ContentUnderConstruction />
+  <div class="flex flex-col flex-1 h-fit min-h-full gap-4 xl:flex-row max-h-full">
+    <div class="flex flex-col gap-6 bg-tertiary p-4 rounded-lg w-full">
+      <div
+        class="flex flex-col items-center gap-2 text-primary self-center text-xl md:flex-row md:text-2xl lg:text-3xl"
+      >
+        <IconBuilderStar class="size-10" />
+
+        {{ $t('experience.work-experience') }}
+      </div>
+
+      <hr class="border-black/20 w-10/12 self-center dark:border-white/15" />
+
+      <ol class="relative overflow-y-active">
+        <ExperienceItem v-for="item in experience" :key="item.position" v-bind="item" />
+      </ol>
+    </div>
+  </div>
 </template>
--- src/views/profile/ProfileView.vue
+++ src/views/profile/ProfileView.vue
@@ -1,18 +1,30 @@
 <script setup lang="ts">
 // import ContentUnderConstruction from '@/components/ContentUnderConstruction.vue'
 import FlipWords from '@/components/inspira-ui/FlipWords.vue'
-import DottedMap from './components/DottedMap.vue'
 import SkillsResume from './components/SkillsResume.vue'
+import { useI18n } from 'vue-i18n'
+import { computed } from 'vue'
+import IconLangs from '@/components/icons/IconLangs.vue'
+
+const { locale } = useI18n()
+
+const whatsappTranslatedMsg = computed(() => {
+  let messageToReturn = '¡Hola, Faber! Visité tu portafolio y me gustaría charlar contigo'
+
+  if (locale.value === 'en') {
+    messageToReturn = 'Hello,  Faber! I visited your portfolio and would like to chat with you'
+  }
+
+  return encodeURIComponent(messageToReturn)
+})
 </script>
 
 <template>
   <div class="flex flex-col flex-1 h-full min-h-full gap-4 xl:flex-row">
-    <div class="relative bg-tertiary rounded-lg w-full xl:w-1/2 xl:h-auto xl:overflow-hidden">
-      <!-- <BorderBeam :size="250" :duration="12" :delay="9" :border-width="2" /> -->
-
-      <div class="flex justify-center dark:text-white p-4 md:p-12 xl:overflow-y-active">
-        <div class="flex flex-col gap-10">
-          <div class="flex flex-col md:flex-row w-full self-center items-center gap-10">
+    <div class="relative bg-tertiary rounded-lg w-full xl:w-1/2 xl:h-auto xl:overflow-y-active">
+      <div class="flex justify-center dark:text-white p-4 md:p-12">
+        <div class="flex flex-col gap-8 w-full">
+          <div class="flex flex-col md:flex-row w-full self-center items-center gap-6">
             <img
               src="/images/profile.avif"
               alt="Profile"
@@ -28,14 +40,21 @@ import SkillsResume from './components/SkillsResume.vue'
 
               <span> Full Stack Developer </span>
 
-              <span>
-                {{ $t('profile.status') }}:
-                <span class="title font-bold">{{ $t('profile.status-value') }}</span>
-              </span>
+              <a
+                download
+                href="https://drive.google.com/uc?export=download&id=1MrfLoPZjHkhIrxQzbhfMdLUqU4ZI-yyR"
+                class="relative font-bold title w-fit cursor-pointer self-center md:self-start"
+              >
+                {{ $t('profile.download-cv') }}
+                <span
+                  class="absolute bottom-0 left-0 w-full h-[2px] title-decorator"
+                  aria-hidden="true"
+                />
+              </a>
             </div>
           </div>
 
-          <div class="flex flex-col 2xl:text-2xl gap-10">
+          <div class="flex flex-col 2xl:text-2xl gap-6">
             <span> {{ $t('profile.description-1') }} </span>
 
             <span> {{ $t('profile.description-2') }} </span>
@@ -54,10 +73,98 @@ import SkillsResume from './components/SkillsResume.vue'
                   'innovation',
                   'optimization',
                 ]"
-                :duration="2000"
+                :duration="1500"
                 class="font-semibold"
               />
             </span>
+          </div>
+
+          <hr class="border-gray-500" />
+
+          <div>
+            <h2 class="title text-xl 2xl:text-2xl font-semibold mb-4">
+              {{ $t('profile.languages') }}
+            </h2>
+
+            <!-- Added grid for languages -->
+            <div class="grid grid-cols-2 gap-4">
+              <div class="flex flex-col items-center">
+                <span class="text-lg font-semibold dark:text-white">
+                  {{ $t('profile.spanish') }}
+                </span>
+                <span class="text-sm dark:text-white">
+                  {{ $t('profile.native') }}
+                </span>
+              </div>
+
+              <div class="flex flex-col items-center">
+                <span class="text-lg font-semibold dark:text-white">
+                  {{ $t('profile.english') }}
+                </span>
+                <span class="text-sm dark:text-white">B1</span>
+              </div>
+            </div>
+          </div>
+
+          <hr class="border-gray-500" />
+
+          <div class="flex flex-col gap-8 2xl:text-xl">
+            <div>
+              <h2 class="title text-xl 2xl:text-2xl font-semibold mb-4">
+                {{ $t('profile.contact-info') }}
+              </h2>
+
+              <ul class="flex flex-col gap-2">
+                <li>
+                  <span class="font-semibold">{{ $t('profile.email') }}: </span>
+                  <span class="font-normal"> fabergrajales1999@gmail.com </span>
+                </li>
+                <li>
+                  <span class="font-semibold">Whatsapp: </span>
+                  <a
+                    class="font-normal underline underline-offset-[5px]"
+                    aria-label="Chat on WhatsApp"
+                    target="_blank"
+                    :href="`https://wa.me/573146310397?text=${whatsappTranslatedMsg}`"
+                  >
+                    (+57) 314 631 0397
+                  </a>
+                </li>
+                <!-- Linkedin -->
+                <li>
+                  <span class="font-semibold">LinkedIn: </span>
+                  <a
+                    class="font-normal underline underline-offset-[5px]"
+                    aria-label="LinkedIn Profile"
+                    target="_blank"
+                    href="https://www.linkedin.com/in/faber-grajales-hincapi%C3%A9-3919b6198/"
+                  >
+                    LinkedIn Profile
+                  </a>
+                </li>
+              </ul>
+            </div>
+
+            <div>
+              <h2 class="title text-xl 2xl:text-2xl font-semibold mb-4">
+                {{ $t('profile.location') }}
+              </h2>
+
+              <ul class="flex flex-col gap-2">
+                <li>
+                  <span class="font-semibold">{{ $t('profile.city') }}: </span>
+                  <span class="font-normal">Armenia (Quindío)</span>
+                </li>
+                <li>
+                  <span class="font-semibold">{{ $t('profile.country') }}: </span>
+                  <span class="font-normal">Colombia</span>
+                </li>
+                <li>
+                  <span class="font-semibold">{{ $t('profile.timezone') }}: </span>
+                  <span class="font-normal">GMT-5</span>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
@@ -65,18 +172,18 @@ import SkillsResume from './components/SkillsResume.vue'
 
     <div class="flex w-full flex-col gap-4 xl:w-1/2 xl:h-auto">
       <div
-        class="flex xl:flex-1 flex-col gap-4 bg-tertiary min-h-96 relative overflow-y-auto rounded-lg justify-evenly items-center dark:text-white p-4 md:min-h-60"
+        class="flex xl:flex-shrink flex-col gap-4 bg-tertiary relative xl:overflow-y-active rounded-lg dark:text-white p-4 md:p-12"
       >
-        <SkillsResume />
-      </div>
+        <div class="flex flex-col gap-6 2xl:text-xl">
+          <h2 class="title text-xl 2xl:text-2xl font-semibold mb-4">
+            {{ $t('profile.availability') }}...
+          </h2>
 
-      <div
-        class="flex xl:flex-1 flex-col gap-4 bg-tertiary min-h-96 relative overflow-hidden rounded-lg justify-evenly items-center dark:text-white p-4 md:min-h-60"
-      >
-        <div class="flex flex-col w-full items-center gap-4 text-xl 2xl:text-2xl">
-          <span class="title font-semibold">{{ $t('profile.availability') }}...</span>
-
-          <ul class="grid w-full text-sm md:text-base 2xl:text-lg gap-2 grid-cols-2 font-medium">
+          <ul class="grid w-full text-sm md:text-base 2xl:text-2xl gap-2 grid-cols-2">
+            <li class="text-nowrap overflow-x-hidden truncate">
+              {{ $t('profile.status') }}:
+              <span class="title font-semibold truncate">{{ $t('profile.status-value') }}!</span>
+            </li>
             <li class="text-nowrap overflow-x-hidden truncate">
               {{ $t('profile.work-type') }}:
               <span class="title font-semibold">{{ $t('profile.work-type-value') }}</span>
@@ -91,33 +198,20 @@ import SkillsResume from './components/SkillsResume.vue'
               {{ $t('profile.relocation') }}:
               <span class="title font-semibold truncate">{{ $t('profile.relocation-value') }}</span>
             </li>
-            <!-- <li class="text-nowrap overflow-x-hidden truncate">
-              {{ $t('profile.status') }}:
-              <span class="title font-semibold truncate">{{ $t('profile.available') }}</span>
-            </li> -->
           </ul>
         </div>
+      </div>
 
-        <div class="h-full aspect-square">
-          <DottedMap />
+      <div
+        class="flex xl:flex-1 flex-col gap-4 bg-tertiary relative xl:overflow-y-active rounded-lg dark:text-white p-4 md:p-12"
+      >
+        <div class="flex flex-col gap-6 2xl:text-xl">
+          <h2 class="title text-xl 2xl:text-2xl font-semibold mb-4">
+            {{ $t('profile.technical-skills') }}
+          </h2>
+          <SkillsResume />
         </div>
       </div>
     </div>
   </div>
 </template>
-
-<style scoped>
-.location-card h2 {
-  margin-bottom: 0.5rem;
-  font-weight: 600;
-}
-
-.location-card ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-.location-card li {
-  margin: 0.25rem 0;
-}
-</style>
